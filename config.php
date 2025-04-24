9b0e7d83a99d30f1927c1d5d0a0e924b31844ec8
Anezus <quocanhnguyen1707@gmail.com>
2025-04-24 08:09:12 +0700

    addf

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'base' => '/PTIT24CNTT01/ss20/',
+    'assets' => '/PTIT24CNTT01/ss20/assets/',
+    'baseURL' => 'http://localhost/PTIT24CNTT01/ss20/',
+    'db' => array(
+        'host' => 'localhost',
+        'name' => 'productdb1',
+        'username' => 'root',
+        'password' => ''
+    )
+);
