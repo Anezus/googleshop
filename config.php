35cc85be4ab4e46aad5754832b516c8bacdfe299
Anezus <quocanhnguyen1707@gmail.com>
2025-04-24 08:13:58 +0700

    ahh

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 return array(
-    'base' => '/PTIT24CNTT01/ss20/',
-    'assets' => '/PTIT24CNTT01/ss20/assets/',
-    'baseURL' => 'http://localhost/PTIT24CNTT01/ss20/',
+    'base' => '/googleshop/',
+    'assets' => '/googleshop/assets/',
+    'baseURL' => 'http://localhost/googleshop/',
     'db' => array(
         'host' => 'localhost',
         'name' => 'productdb1',
